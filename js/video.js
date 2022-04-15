5478ff0acd242b34c94d8ef563ad5ba09d210de3
yuxwang26 <97485240+yuxwang26@users.noreply.github.com>
2022-04-15 16:50:18 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -32,8 +32,8 @@ document.querySelector("#faster").addEventListener("click", function() {
 
 document.querySelector("#skip").addEventListener("click", function() {
 	console.log("Skip Ahead")
-	video.currentTime + 15
-	if(video.currentTime > video.duration){
+	video.currentTime += 15
+	if(video.currentTime >= video.duration){
 		video.currentTime = 0
 	}
 	console.log("Current time is " + video.currentTime)
